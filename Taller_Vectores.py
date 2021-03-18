b8045aab0e4170e6d7d679f13fb041ee01046e6f
Osnaider Carbonell <42350856+osnacarbonelles@users.noreply.github.com>
2021-03-18 18:46:26 -0500

    Solucion ejercicio 2

--- Taller_Vectores.py
+++ Taller_Vectores.py
@@ -64,6 +64,51 @@ mayor(listaNumeros)
 menor(listaNumeros)
 
 
+"""
+2. De los n elementos de un vector dado calcule:
+a. Cantidad de elementos pares
+b. Cantidad de elementos impares
+c. Cantidad de elementos primos
+"""
+i = 1
+listaNumeros = []
+while True:
+    numero = int(input(f'Digite cualquier número. Si desea salir digite 0: '))
+    if numero == 0:
+        break
+    listaNumeros.append(numero)
+    i += 1
+print(f'Los numeros ingresados fueron: {listaNumeros}')
+
+
+def par(listaNumeros):
+    cont_par = 0
+    for i in listaNumeros:
+        if i % 2 == 0:
+            cont_par = cont_par + 1
+    return print(f'La cantidad de elementos pares es: {cont_par}')
+
+
+def impar(listaNumeros):
+    cont_impar = 0
+    for i in listaNumeros:
+        if i % 2 != 0:
+            cont_impar = cont_impar + 1
+    return print(f'La cantidad de elementos impares es: {cont_impar}')
+
+
+def primos(listaNumeros):
+    cont_primos = 0
+    for i in listaNumeros:
+        if i % 2 == 1:
+            cont_primos = cont_primos + 1
+    return print(f'La cantidad de elementos primos es: {cont_primos}')
+
+
+par(listaNumeros)
+impar(listaNumeros)
+primos(listaNumeros)
+
 
 
 
