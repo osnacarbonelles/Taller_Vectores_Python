26091f323d2405b0108d8b62075bd6a6a4fc2b06
Osnaider Carbonell <42350856+osnacarbonelles@users.noreply.github.com>
2021-03-18 19:16:11 -0500

    Solucion ejercicio 6

--- Taller_Vectores.py
+++ Taller_Vectores.py
@@ -216,6 +216,34 @@ productoria(listaNumerosB)  # se utiliza la funcion del primer ejercicio
 sumatoria(listaNumerosC)  # se utiliza la funcion del primer ejercicio
 
 
+"""
+6. Dado un vector v, indique si es simétrico, un vector es simétrico si siendo
+longitud par los números de la primera mitad son iguales al inverso de la
+otra mitad por ejemplo: X=[1,2,3,3,2,1], en el ejemplo x es un vector
+simétrico, en caso que la longitud del vector sea impar, se ignorará el
+elemento central y se seguirá la misma lógica anterior, por ejemplo:
+Y=[3,5,7,8,7,5,3], en este ejemplo Y es simétrico.
+"""
+i = 1
+listaNumeros = []
+while True:
+    numero = int(input(f'Digite cualquier número. Si desea salir digite 0: '))
+    if numero == 0:
+        break
+    listaNumeros.append(numero)
+    i += 1
+print(f'Los numeros ingresados fueron: {listaNumeros}')
+
+
+def simetrico(listaNumeros):
+    if listaNumeros == listaNumeros[:: -1]:
+        print("Si es simétrico")
+    else:
+        print("No es simétrico")
+
+
+simetrico(listaNumeros)
+
 
 
 
